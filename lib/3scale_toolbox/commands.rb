446e7c3d86811f09e80388a8dee6da56421bd586
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-07-11 15:19:36 +0200

    plugin framework: builtin commands

--- lib/3scale_toolbox/commands.rb
+++ lib/3scale_toolbox/commands.rb
@@ -1,8 +1,16 @@
 require '3scale_toolbox/commands/3scale_command'
+require '3scale_toolbox/commands/help_command'
+require '3scale_toolbox/commands/copy_command'
+require '3scale_toolbox/commands/import_command'
+require '3scale_toolbox/commands/update_command'
 
 module ThreeScaleToolbox
   module Commands
     BUILTIN_COMMANDS = [ # :nodoc:
+      ThreeScaleToolbox::Commands::HelpCommand,
+      ThreeScaleToolbox::Commands::CopyCommand,
+      ThreeScaleToolbox::Commands::ImportCommand,
+      ThreeScaleToolbox::Commands::UpdateCommand
     ].freeze
   end
 end
