954ca1cbe4298166b52416e1cccd90bf981c1e81
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-07-31 13:39:37 +0200

    spec/unit/3scale_toolbox_spec: test 3scale_toolbox main module

---
+++ spec/unit/3scale_toolbox_spec.rb
@@ -0,0 +1,33 @@
+require 'fileutils'
+require 'shared_contexts'
+require '3scale_toolbox'
+
+RSpec.describe ThreeScaleToolbox do
+  include_context :temp_dir
+  include_context :plugin
+  include_context :random_name
+
+  let(:name) { random_lowercase_name }
+  let(:dest_plugin_file) { File.join(tmp_dir, '3scale_toolbox_plugin.rb') }
+
+  before(:each) do
+    plugin = get_plugin_content(name.capitalize, name)
+    File.open(dest_plugin_file, 'w') do |file|
+      file.write(plugin)
+    end
+    $LOAD_PATH.unshift(tmp_dir) unless $LOAD_PATH.include?(tmp_dir)
+  end
+
+  after(:each) do
+    $LOAD_PATH.delete(tmp_dir)
+  end
+
+  it '.plugin_paths finds plugin' do
+    expect(described_class.plugin_paths).to include(dest_plugin_file)
+  end
+
+  it '.load_plugins loads plugin' do
+    expect { described_class.load_plugins }.not_to raise_error
+    expect(Object.const_get(name.capitalize.to_sym)).not_to be_nil
+  end
+end
