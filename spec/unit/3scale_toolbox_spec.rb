f7e4be1b9ec04f2f143722ab60f6dad3e96d3412
Eguzki Astiz Lezaun <eguzki.astiz@gmail.com>
2018-08-02 23:11:09 +0200

    spec/unit/3scale_toolbox_spec: tmp dir as pathname object

--- spec/unit/3scale_toolbox_spec.rb
+++ spec/unit/3scale_toolbox_spec.rb
@@ -6,24 +6,22 @@ RSpec.describe ThreeScaleToolbox do
   include_context :random_name
 
   let(:name) { random_lowercase_name }
-  let(:dest_plugin_file) { File.join(tmp_dir, '3scale_toolbox_plugin.rb') }
+  let(:dest_plugin_file) { tmp_dir.join('3scale_toolbox_plugin.rb') }
 
   around(:each) do |example|
     plugin = get_plugin_content(name.capitalize, name)
-    File.open(dest_plugin_file, 'w') do |file|
-      file.write(plugin)
-    end
+    dest_plugin_file.write(plugin)
     $LOAD_PATH.unshift(tmp_dir) unless $LOAD_PATH.include?(tmp_dir)
     example.run
     $LOAD_PATH.delete(tmp_dir)
   end
 
   it '.plugin_paths finds plugin' do
-    expect(described_class.plugin_paths).to include(dest_plugin_file)
+    expect(described_class.plugin_paths).to include(dest_plugin_file.to_s)
   end
 
   it '.load_plugins loads plugin' do
     expect { described_class.load_plugins }.not_to raise_error
-    expect(Object.const_get(name.capitalize.to_sym)).not_to be_nil
+    expect(Object.const_get(name.capitalize.to_sym)).to be_a_kind_of(ThreeScaleToolbox::Command)
   end
 end
