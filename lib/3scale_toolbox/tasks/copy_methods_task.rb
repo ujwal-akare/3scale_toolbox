3ed3fbf11773f5d934bff8c26c78f0c7fc394617
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-11-21 18:28:25 +0100

    tasks/copy_methods_task: refactor

--- lib/3scale_toolbox/tasks/copy_methods_task.rb
+++ lib/3scale_toolbox/tasks/copy_methods_task.rb
@@ -5,18 +5,23 @@ module ThreeScaleToolbox
 
       def call
         source_methods = source.methods
-        copy_methods = target.methods
+        target_methods = target.methods
+        target_hits_metric_id = target.hits['id']
         puts "original service hits metric #{source.hits['id']} has #{source_methods.size} methods"
-        puts "copied service hits metric #{target.hits['id']} has #{copy_methods.size} methods"
-        missing_methods = ThreeScaleToolbox::Helper.array_difference(source_methods, copy_methods) do |method, copy|
-          ThreeScaleToolbox::Helper.compare_hashes(method, copy, ['system_name'])
-        end
-        puts "creating #{missing_methods.size} missing methods on copied service"
-
-        missing_methods.each do |method|
+        puts "target service hits metric #{target_hits_metric_id} has #{target_methods.size} methods"
+        missing = missing_methods(source_methods, target_methods).each do |method|
           filtered_method = ThreeScaleToolbox::Helper.filter_params(%w[friendly_name system_name],
                                                                     method)
-          target.create_method(target.hits['id'], filtered_method)
+          target.create_method(target_hits_metric_id, filtered_method)
+        end
+        puts "created #{missing.size} missing methods on target service"
+      end
+
+      private
+
+      def missing_methods(source_methods, target_methods)
+        ThreeScaleToolbox::Helper.array_difference(source_methods, target_methods) do |method, target|
+          ThreeScaleToolbox::Helper.compare_hashes(method, target, ['system_name'])
         end
       end
     end
