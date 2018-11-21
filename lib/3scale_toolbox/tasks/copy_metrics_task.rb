53a5f48f465ff2a2fcfacfc132365a7ef9d56b23
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-11-21 18:43:12 +0100

    copy_metrics_task: refactor variable names

--- lib/3scale_toolbox/tasks/copy_metrics_task.rb
+++ lib/3scale_toolbox/tasks/copy_metrics_task.rb
@@ -5,26 +5,27 @@ module ThreeScaleToolbox
 
       def call
         source_metrics = source.metrics
-        copy_metrics = target.metrics
+        target_metrics = target.metrics
 
         puts "original service has #{source_metrics.size} metrics"
-        puts "copied service has #{copy_metrics.size} metrics"
+        puts "target service has #{target_metrics.size} metrics"
 
-        missing_metrics = missing_metrics(source_metrics, copy_metrics)
+        missing = missing_metrics(source_metrics, target_metrics)
 
-        missing_metrics.each do |metric|
+        missing.each do |metric|
           metric.delete('links')
           target.create_metric(metric)
         end
 
-        puts "created #{missing_metrics.size} metrics on the copied service"
+        puts "created #{missing.size} metrics on the target service"
       end
 
       private
 
-      def missing_metrics(source_metrics, copy_metrics)
-        ThreeScaleToolbox::Helper.array_difference(source_metrics, copy_metrics) do |metric, copy|
-          ThreeScaleToolbox::Helper.compare_hashes(metric, copy, ['system_name'])
+      def missing_metrics(source_metrics, target_metrics)
+        ThreeScaleToolbox::Helper.array_difference(source_metrics,
+                                                   target_metrics) do |source, target|
+          ThreeScaleToolbox::Helper.compare_hashes(source, target, ['system_name'])
         end
       end
     end
