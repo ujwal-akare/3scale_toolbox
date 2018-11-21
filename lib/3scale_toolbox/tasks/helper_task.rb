9d2483c80bc6a1aab12680e91b6607cc630c6a18
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-11-21 16:43:48 +0100

    tasks/copy_limits_task, tasks/helper_task: refactor var name

--- lib/3scale_toolbox/tasks/helper_task.rb
+++ lib/3scale_toolbox/tasks/helper_task.rb
@@ -1,23 +1,24 @@
 module ThreeScaleToolbox
   module Tasks
     module Helper
-      def metrics_mapping(metrics, copy_metrics)
-        copy_metrics.map do |copy|
-          metric = metrics.find do |m|
-            ThreeScaleToolbox::Helper.compare_hashes(m, copy, ['system_name'])
+      def metrics_mapping(source_metrics, target_metrics)
+        target_metrics.map do |target|
+          source = source_metrics.find do |m|
+            ThreeScaleToolbox::Helper.compare_hashes(m, target, ['system_name'])
           end || {}
 
-          [metric['id'], copy['id']]
+          [source['id'], target['id']]
         end.to_h
       end
 
-      def application_plan_mapping(app_plans, copy_app_plans)
-        copy_app_plans.map do |copy|
-          plan = app_plans.find do |p|
-            ThreeScaleToolbox::Helper.compare_hashes(p, copy, ['system_name'])
-          end
-          [plan['id'], copy['id']]
+      def application_plan_mapping(source_app_plans, target_app_plans)
+        mapping = target_app_plans.map do |target|
+          source = source_app_plans.find do |app_plan|
+            ThreeScaleToolbox::Helper.compare_hashes(app_plan, target, ['system_name'])
+          end || {}
+          [source['id'], target['id']]
         end
+        mapping.reject { |key, _| key.nil? }
       end
     end
   end
