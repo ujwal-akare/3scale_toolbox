091fc1e47754e819eb146384d65000a8f19aacbd
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-03-06 13:10:50 +0100

    import openapi: fix when missing operation id in spec

--- lib/3scale_toolbox/commands/import_command/openapi/method.rb
+++ lib/3scale_toolbox/commands/import_command/openapi/method.rb
@@ -12,11 +12,15 @@ module ThreeScaleToolbox
           end
 
           def friendly_name
-            operation[:operationId]
+            operation[:operationId] || operation_id
           end
 
           def system_name
             friendly_name.downcase
+          end
+
+          def operation_id
+            "#{operation[:verb]}#{operation[:path].gsub(/[^\w]/, '')}"
           end
         end
       end
