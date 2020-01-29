28b20007f5e9d91814c8aa4961eed5406c364145
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-01-29 17:19:39 +0100

    structured output: proxy_config show command

--- lib/3scale_toolbox/commands/proxy_config_command/show_command.rb
+++ lib/3scale_toolbox/commands/proxy_config_command/show_command.rb
@@ -11,41 +11,39 @@ module ThreeScaleToolbox
               usage       'show <remote> <service> <environment>'
               summary     'Show Proxy Configuration'
               description 'Show a Proxy Configuration'
-              runner ShowSubcommand
 
               param   :remote
               param   :service_ref
               param   :environment
 
-              option nil, :'config-version', "Specify the Proxy Configuration version. If not specified it gets the latest version", argument: :required
+              ThreeScaleToolbox::CLI.output_flag(self)
+              option nil, :'config-version', "Specify the Proxy Configuration version. If not specified it gets the latest version", default: 'latest', argument: :required
+
+              runner ShowSubcommand
             end
           end
 
-          def run      
-            print_proxy_config
+          def run
+            printer.print_record proxy_config.attrs
           end
 
           private
 
           def proxy_config
-            @proxy_config ||= find_proxy_config 
-          end
-
-          def find_proxy_config
-            if proxy_config_version == "latest"
-              find_proxy_config_latest
+            if proxy_config_version_option == 'latest'
+              proxy_config_latest
             else
-              find_proxy_config_version
+              proxy_config_version
             end
           end
 
-          def find_proxy_config_version
-            Entities::ProxyConfig.find(service: service, environment: proxy_config_environment, version: proxy_config_version).tap do |pc|
+          def proxy_config_version
+            Entities::ProxyConfig.find(service: service, environment: proxy_config_environment, version: proxy_config_version_option).tap do |pc|
               raise ThreeScaleToolbox::Error, "ProxyConfig #{proxy_config_environment} in service #{service.id} does not exist" if pc.nil?
             end
           end
 
-          def find_proxy_config_latest
+          def proxy_config_latest
             Entities::ProxyConfig.find_latest(service: service, environment: proxy_config_environment).tap do |pc|
               raise ThreeScaleToolbox::Error, "ProxyConfig #{proxy_config_environment} in service #{service.id} does not exist" if pc.nil?
             end
@@ -55,16 +53,12 @@ module ThreeScaleToolbox
             @remote ||= threescale_client(arguments[:remote])
           end
 
-          def proxy_config_version
-            options[:'config-version'] || "latest"
+          def proxy_config_version_option
+            options[:'config-version']
           end
 
           def proxy_config_environment
             arguments[:environment]
-          end
-
-          def print_proxy_config
-            puts JSON.pretty_generate(proxy_config.attrs)    
           end
 
           def service_ref
@@ -72,14 +66,17 @@ module ThreeScaleToolbox
           end
 
           def find_service
-            Entities::Service.find(remote: remote,
-                                   ref: service_ref).tap do |svc|
+            Entities::Service.find(remote: remote, ref: service_ref).tap do |svc|
               raise ThreeScaleToolbox::Error, "Service #{service_ref} does not exist" if svc.nil?
             end
           end
 
           def service
             @service ||= find_service
+          end
+
+          def printer
+            options.fetch(:output, CLI::JsonPrinter.new)
           end
         end
       end
