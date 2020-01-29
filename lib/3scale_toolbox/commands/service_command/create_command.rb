707c2f4f5b45c487545d5882c92cae214017488b
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-01-29 16:44:49 +0100

    structured output: service create command

--- lib/3scale_toolbox/commands/service_command/create_command.rb
+++ lib/3scale_toolbox/commands/service_command/create_command.rb
@@ -2,6 +2,16 @@ module ThreeScaleToolbox
   module Commands
     module ServiceCommand
       module Create
+        class CustomPrinter
+          attr_reader :option_default, :option_disabled
+
+          def print_record(service)
+            puts "Service '#{service['name']}' has been created with ID: #{service['id']}"
+          end
+
+          def print_collection(collection) end
+        end
+
         class CreateSubcommand < Cri::CommandRunner
           include ThreeScaleToolbox::Command
 
@@ -11,23 +21,24 @@ module ThreeScaleToolbox
               usage       'create [options] <remote> <service-name>'
               summary     'Create a service'
               description 'Create a service'
-              runner CreateSubcommand
 
               param   :remote
               param   :service_name
 
+              ThreeScaleToolbox::CLI.output_flag(self)
               option :d, :'deployment-mode', "Specify the deployment mode of the service", argument: :required
               option :s, :'system-name', "Specify the system-name of the service", argument: :required
               option :a, :'authentication-mode', "Specify authentication mode of the service ('1' for API key, '2' for App Id / App Key, 'oauth' for OAuth mode, 'oidc' for OpenID Connect)", argument: :required
               option nil, :description, "Specify the description of the service", argument: :required
               option nil, :'support-email', "Specify the support email of the service", argument: :required
+
+              runner CreateSubcommand
             end
           end
 
           def run
-            create_service_params = service_attrs
-            result = Entities::Service.create(remote: remote, service_params: create_service_params)
-            puts "Service '#{arguments[:service_name]}' has been created with ID: #{result.id}"
+            service = Entities::Service.create(remote: remote, service_params: create_params)
+            printer.print_record service.attrs
           end
 
           private
@@ -46,11 +57,16 @@ module ThreeScaleToolbox
             }.compact
           end
 
-          def service_attrs
+          def create_params
             service_name = arguments[:service_name]
             create_service_attrs = parse_options
-            create_service_attrs["name"] = service_name
+            create_service_attrs['name'] = service_name
             create_service_attrs
+          end
+
+          def printer
+            # keep backwards compatibility
+            options.fetch(:output, CustomPrinter.new)
           end
         end
       end
