e5be8dd843e120e4cd8bf6bd086061b54e265a7c
Eguzki Astiz Lezaun <eastizle@redhat.com>
2021-02-15 19:53:50 +0100

    fix delete_active_docs_task

--- spec/unit/commands/update_command/service_command/delete_activedocs_task_spec.rb
+++ spec/unit/commands/update_command/service_command/delete_activedocs_task_spec.rb
@@ -2,25 +2,21 @@ RSpec.describe ThreeScaleToolbox::Commands::UpdateCommand::ServiceCommand::Delet
   context '#call' do
     let(:target) { instance_double('ThreeScaleToolbox::Entities::Service', 'target') }
     let(:remote) { instance_double('ThreeScale::API::Client', 'remote') }
+    let(:activedocs0) { instance_double(ThreeScaleToolbox::Entities::ActiveDocs, 'activedocs0') }
+    let(:activedocs1) { instance_double(ThreeScaleToolbox::Entities::ActiveDocs, 'activedocs1') }
+    let(:activedocs2) { instance_double(ThreeScaleToolbox::Entities::ActiveDocs, 'activedocs2') }
+    let(:target_activedocs) { [activedocs0, activedocs1, activedocs2] }
     subject { described_class.new(target: target) }
 
     before :each do
       allow(target).to receive(:remote).and_return(remote)
+      expect(target).to receive(:activedocs).and_return(target_activedocs)
     end
 
     context 'several activedocs available' do
-      let(:n_activedocs) { 10 }
-      let(:target_activedocs) do
-        Array.new(n_activedocs) { |idx| { 'id' => idx } }
-      end
 
-      it 'it calls delete_activedocs method on each activedocs' do
-        expect(target).to receive(:activedocs).and_return(target_activedocs)
-        expect(target_activedocs.size).to be > 0
-        target_activedocs.each do |activedocs|
-          expect(remote).to receive(:delete_activedocs).with(activedocs['id'])
-        end
-
+      it 'it calls delete method on each activedocs' do
+        target_activedocs.each { |activedocs| expect(activedocs).to receive(:delete) }
         # Run
         subject.call
       end
