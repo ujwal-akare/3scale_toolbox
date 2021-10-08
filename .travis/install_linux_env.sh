5b48e18c0dc9469a2c179e9f0180f2f2bff7ea2a
Artiom Vaskov <artiom.vaskov@ibm.com>
2021-10-08 11:55:34 -0400

    Better naming for testing script. '--add-platform' removed (platforms already in the lock file).

--- .travis/install_linux_env.sh
+++ .travis/install_linux_env.sh
@@ -3,8 +3,4 @@
 set -ev
 
 bundle config --local set path 'vendor/bundle'
-case "${TRAVIS_CPU_ARCH}" in
-  ppc64le) bundle lock --add-platform powerpc64le-linux;;
-    s390x) bundle lock --add-platform s390x-linux;;
-esac
 bundle install --jobs=3 --retry=3
